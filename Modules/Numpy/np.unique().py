aefc6eabb16802383565097cef48649230892bea
SanketWagh <firearmz0147@gmail.com>
2021-11-21 09:53:13 +0530

    Studied pd.get_dummies, gradient descent and modified np.unique, creaed a code to find multiple modes in a give data Series

---
+++ Modules/Numpy/np.unique().py
@@ -0,0 +1,17 @@
+def top_modes(a_list):
+    import numpy as np
+    unique, index, counts = np.unique(a_list, return_counts=True, return_index= True)
+    # print(unique, "unique")
+    # print(counts, "counts")
+    # print(index, "index")
+    c = a_list.count(a_list[1])
+    # print(c)
+    r = [ unique[i] for i in range(len(unique)) if counts[i] == max(counts) ]
+    # print(r)
+    for i in r:
+        print("Modes are:", i)
+    
+    
+a_list = [1,1,2,2,3,4,4]
+
+top_modes(a_list)
