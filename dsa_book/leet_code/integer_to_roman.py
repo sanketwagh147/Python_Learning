ad3d6290fcc01c89f97d1bb864d416764d7c0d53
Sanket Wagh <sanketwagh147@gmail.com>
2025-03-20 08:04:26 +0530

    daily coding challenge 38/180

---
+++ dsa_book/leet_code/integer_to_roman.py
@@ -0,0 +1,28 @@
+"Leet code 12 Integer to roman"
+
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [
+            (1000, "M"),
+            (900, "CM"),
+            (500, "D"),
+            (400, "CD"),
+            (100, "C"),
+            (90, "XC"),
+            (50, "L"),
+            (40, "XL"),
+            (10, "X"),
+            (9, "IX"),
+            (5, "V"),
+            (4, "IV"),
+            (1, "I"),
+        ]
+
+        result = []
+        for value, symbol in values:
+            while num >= value:
+                num -= value
+                result.append(symbol)
+
+        return "".join(result)
